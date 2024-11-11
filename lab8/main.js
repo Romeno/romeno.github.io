72d170d0ac9fd7a7277dd0b77bf57c8f30c07a0f
Sergio Rusu <sergiorusu0@gmail.com>
2024-11-11 18:28:55 +0000

    Lab 8 terminado

--- lab8/main.js
+++ lab8/main.js
@@ -1,104 +1,92 @@
-//1
-
 document.addEventListener("DOMContentLoaded", () => {
+  //1
+  const frase = document.querySelector("#mensagem");
 
-const frase = document.querySelector("#mensagem");
+  const mudaFrase = () => {
+    if (frase.innerHTML === "1. Passa por aqui!") {
+      frase.innerHTML = "1. Obrigado por passares!";
+    }
+  };
+  const restauraFrase = () => {
+    frase.innerHTML = "1. Passa por aqui!";
+  };
 
-const mudaFrase = () => {
-  if (frase.innerHTML === "1. Passa por aqui!") {
-    frase.innerHTML = "1. Obrigado por passares!";
+  frase.addEventListener("mouseover", mudaFrase);
+  frase.addEventListener("mouseout", restauraFrase);
+
+  //2
+  const pintaMe = document.querySelector("#pintar");
+  const colorButtons = document.querySelectorAll(".color-button");
+
+  function alteraCor(event) {
+    const color = event.target.dataset.color;
+    pintaMe.style.color = color;
   }
-};
-const restauraFrase = () => {
-  frase.innerHTML = "1. Passa por aqui!";
-};
 
-frase.addEventListener("mouseover", mudaFrase);
-frase.addEventListener("mouseout", restauraFrase);
-});
+  colorButtons.forEach((button) => {
+    button.addEventListener("click", alteraCor);
+  });
 
+  //3
+  const texto = document.querySelector("#texto3");
+  const cores = ["red", "green", "blue", "yellow", "gray"];
+  let colorIndex = 0;
 
+  function alteraCorBackground() {
+    texto.style.backgroundColor = cores[colorIndex];
+    colorIndex = (colorIndex + 1) % cores.length;
+  }
 
-//2
-const pintaMe = document.querySelector("#pintar");
-const colorButtons = document.querySelectorAll(".color-button");
+  texto.addEventListener("input", alteraCorBackground);
+  texto.addEventListener("change", alteraCorBackground);
 
-function alteraCor() {
-  const color = colorButtons.dataset.color;
-  pintaMe.style.color = color;
-}
+  //4
+  document.querySelector("select").onchange = function () {
+    document.querySelector("body").style.backgroundColor = this.value;
+  };
 
-colorButtons.forEach((button) => {
-  button.addEventListener("click", alteraCor);
-});
+  //5
+  if (!localStorage.getItem("counter")) {
+    localStorage.setItem("counter", 0);
+  }
 
+  const butao = document.querySelector(".contar");
+  const numero = document.querySelector("#numero");
 
+  let counter = Number(localStorage.getItem("counter"));
 
-//3
-const texto = document.querySelector("#texto3");
-const cores = ["red", "green", "blue", "yellow", "gray"];
-let colorIndex = 0;
-
-function alteraCorBackground() {
-  texto.style.backgroundColor = cores[colorIndex];
-  colorIndex = (colorIndex + 1) % cores.length;
-}
-
-texto.addEventListener("input", alteraCorBackground);
-texto.addEventListener("change", alteraCorBackground);
-
-
-
-//4
-
-document.querySelector("select").onchange = function () {
-document.querySelector("body").style.backgroundColor = this.value;
-};
-
-
-
-//5
-if (!localStorage.getItem("counter")) {
-  localStorage.setItem("counter", 0);
-}
-
-const butao = document.querySelector(".contar");
-const numero = document.querySelector("#numero");
-
-let counter = Number(localStorage.getItem("counter"));
-
-numero.innerText = counter;
-
-function aumentaNumero() {
-  counter++;
   numero.innerText = counter;
-  localStorage.setItem("counter", counter);
-}
 
-butao.addEventListener("click", aumentaNumero);
+  function aumentaNumero() {
+    counter++;
+    numero.innerText = counter;
+    localStorage.setItem("counter", counter);
+  }
 
+  butao.addEventListener("click", aumentaNumero);
 
+  //6
+  const nome = document.querySelector("#nome");
+  const idade = document.querySelector("#idade");
+  const dados = document.querySelector("#dados");
+  const form = document.querySelector("#myForm");
 
-//6
+  form.onsubmit = (e) => {
+    e.preventDefault();
+    dados.innerHTML = `Olá, o ${nome.value} tem ${idade.value}!`;
+  };
 
-const nome = document.querySelector("#nome");
-const idade = document.querySelector("#idade");
-const dados = document.querySelector("#dados");
-const form = document.querySelector("#myForm");
+  //7
+  if (!localStorage.getItem("counterSpan")) {
+    localStorage.setItem("counterSpan", 0);
+  }
 
-document.querySelector("form").onsubmit = (e) => {
-  e.preventDefault(); // Prevent the form from submitting
-  dados.innerHTML = `Olá, o ${nome.value} tem ${idade.value}!`;
-};
-
-form.onsubmit = escreveDados;
-
-
-//7
-
-let counterSpan = 0;
-const span = document.querySelector("span");
-function count() {
-  span.innerHTML = ++counterSpan;
-}
-setInterval(count, 1000);
+  const span = document.querySelector("span");
+  function count() {
+    let counterSpan = Number(localStorage.getItem("counterSpan"));
+    counterSpan++;
+    span.innerHTML = counterSpan;
+    localStorage.setItem("counterSpan", counterSpan);
+  }
+  setInterval(count, 1000);
+});
